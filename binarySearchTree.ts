7ab22c1fc2207670593eb4fa38c23970cd9b23a7
Garret O'Connor <okenerenterprises@gmail.com>
2023-08-26 13:47:55 -0400

    height, depth, and isBalanced

--- binarySearchTree.ts
+++ binarySearchTree.ts
@@ -1,3 +1,5 @@
+import { ro } from "date-fns/locale";
+
 class TreeNode<E> {
     value: E;
     left: TreeNode<E> | null;
@@ -110,5 +112,102 @@ class Tree<E> {
         this.root = this.deleteRec(this.root, value);
         return this.root;
     }
-}
 
+    levelOrderRec(queue: TreeNode<E>[], acc: E[], fn?: (x: E) => E): E[] {
+        if (queue.length < 1) return acc;
+        
+        const func = fn ?? ((x: E): E => x);
+
+        let nextQueue: TreeNode<E>[] = [];
+        let nextAcc = [...acc];
+
+        queue.forEach((item: TreeNode<E>) => {
+            if (item.left) nextQueue.push(item.left);
+            if (item.right) nextQueue.push(item.right);
+            nextAcc.push(func(item.value));
+        });
+
+        return this.levelOrderRec(nextQueue, nextAcc, func);
+    }
+
+    levelOrder(fn?: (x: E) => E): E[] | null {
+        if (!this.root) return null;
+        else return this.levelOrderRec([this.root], [], fn);
+    }
+
+
+    // Write inorder, preorder, and postorder functions that accept a function parameter. 
+    // Each of these functions should traverse the tree in their respective depth-first order 
+    // and yield each node to the provided function given as an argument. 
+    // The functions should return an array of values if no function is given.
+
+    // inOrder: Left, Root, Right.
+    // This means the LEFTMOST node is the FIRST node.
+    // Let's implement this recursively.
+    // Inputs: root: TreeNode<E>, acc: E[], fn?: (x: E) => E)
+    // Outputs: E[]
+    // Case 1: root.left is null and root.right is not null
+    // -> apply fn to root, add to accumulator
+    // -> recursively call function on root.right
+    // Case 2: root.left is not null
+    // -> recursively call function on root.left
+    // Case 3: root.left and root.right are both null
+    // -> apply fn to root, add to accumulator
+    // -> go back up the chain?
+
+    inOrderRec(root: TreeNode<E> | null, acc: E[], fn?: (x: E) => E): E[] | null {
+        //WIP
+
+        if (!root) return null;
+
+        const func = fn ?? ((x: E): E => x);
+         
+        if (root.left) {
+            this.inOrderRec(root.left, acc, func);
+        }   
+
+        return acc;
+    }
+
+    heightRec(root: TreeNode<E> | null): number {
+        if (!root) return -1;
+        else if (!root.left && !root.right) return 1;
+        else if (!root.left && root.right) return this.heightRec(root.right);
+        else if (!root.right && root.left) return this.heightRec(root.left);
+        else return Math.max(this.heightRec(root.left), this.heightRec(root.right)) + 1
+    }
+
+    height(): number {
+        return this.heightRec(this.root);
+    }
+
+    depthRec(node: TreeNode<E> | null, root: TreeNode<E> | null): number {
+        if (!node || !root) return -1;
+        else if (node.value === root.value) return 0;
+        else if (!root.left || node.value > root.value) return 1 + this.depthRec(node, root.right);
+        else if (!root.right || node.value <  root.value) return 1 + this.depthRec(node, root.left);
+        else return -1;
+    }
+
+    depth(node: TreeNode<E> | null): number {
+        return this.depthRec(node, this.root);
+    }
+
+    isBalancedRec(root: TreeNode<E> | null): boolean {
+        if (!root) return false;
+        else if (!root.left && this.heightRec(root.right) <= 1) return true;
+        else if (!root.right && this.heightRec(root.left) <= 1) return true;
+        else if (!root.left || !root.right) return false;
+
+        const smallHeightDifference = Math.abs(this.heightRec(root.left) - this.heightRec(root.right)) <= 1;
+        return smallHeightDifference && this.isBalancedRec(root.left) && this.isBalancedRec(root.right);
+    }
+
+    isBalanced(): boolean {
+        return this.isBalancedRec(this.root);
+    }
+
+    rebalance() {
+        //WIP
+    }
+}
